78d3ee3a37eb9de93a0bbd445711cd80fefb53ab
ranjayzheng <ranjayzheng@qq.com>
2018-12-27 15:38:32 +0800

    extend DELAY_AFTER_CLICK_START_BUTTON

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const {
 
 const TOTAL_TIMES_FOR_ACCELERATE = 12
 const ACCELERATE_DURATION = 5600
-const DELAY_AFTER_CLICK_START_BUTTON = 500 
+const DELAY_AFTER_CLICK_START_BUTTON = 1000 
 let times = 2
 
 exec('adb shell input tap 500 1780')
