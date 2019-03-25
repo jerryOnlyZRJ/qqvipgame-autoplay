209e5205866b750507e2bf0e85f2dabf092d0bb3
ranjayzheng <ranjayzheng@qq.com>
2019-03-25 08:09:27 +0800

    update 4 meizu X8

--- index.js
+++ index.js
@@ -2,23 +2,31 @@ const {
     exec
 } = require('child_process')
 
+// 一局游戏需要加速的次数
 const TOTAL_TIMES_FOR_ACCELERATE = 15
-const ACCELERATE_PRESS_DURATION = 270
-const ACCELERATE_DURATION = 5600
-const DELAY_AFTER_CLICK_START_BUTTON = 1000 
+// 长按加速按钮的时间
+const ACCELERATE_PRESS_DURATION = 250
+// 两次按下加速按钮的时间间隔
+const ACCELERATE_DURATION = 5800
+// 按下开始按钮后的延迟
+const DELAY_AFTER_CLICK_START_BUTTON = 1000
+// 开始/加速按钮位置
+const START_BTN_POSITION = [545, 2060]
+// 继续按钮位置
+const NEXT_BTN_POSITION = [545, 1340]
 let times = 2
 
-exec('adb shell input tap 500 1780')
+exec(`adb shell input tap ${START_BTN_POSITION[0]} ${START_BTN_POSITION[1]}`)
 setTimeout(() => {
-    exec(`adb shell input swipe 500 1780 501 1781 ${ACCELERATE_PRESS_DURATION}`)
+    exec(`adb shell input swipe ${START_BTN_POSITION[0]} ${START_BTN_POSITION[1]} ${START_BTN_POSITION[0] + 1} ${START_BTN_POSITION[1] + 1} ${ACCELERATE_PRESS_DURATION}`)
     const loops = setInterval(() => {
         if (times > TOTAL_TIMES_FOR_ACCELERATE) {
             clearInterval(loops)
-            exec('adb shell input tap 535 1050')
+            exec(`adb shell input tap ${NEXT_BTN_POSITION[0]} ${NEXT_BTN_POSITION[1]}`)
             process.exit(0)
         } else {
             // 长按加速按钮
-            exec(`adb shell input swipe 500 1780 501 1781 ${ACCELERATE_PRESS_DURATION}`)
+            exec(`adb shell input swipe ${START_BTN_POSITION[0]} ${START_BTN_POSITION[1]} ${START_BTN_POSITION[0] + 1} ${ACCELERATE_PRESS_DURATION}`)
             times++
         }
     }, ACCELERATE_DURATION)
